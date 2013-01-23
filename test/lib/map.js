41e19b19c642f2f60c840ec00f3cdab2dd37b7ea
Thorsten Lorenz <thlorenz@gmx.de>
2013-01-23 07:53:37 -0500

    adding documentation to map module and making API more consistent (only work with string or array of chars)

--- test/lib/map.js
+++ test/lib/map.js
@@ -40,6 +40,62 @@ test('\nkey combinations in insert mode', function (t) {
     map.insert('ctrl-i', 'escape')
 
     t.equal(map.mappings.insert.immediates['ctrl-i'], 'escape', 'it is added to the mappings under insert.immediates.ctrl-i')
+    t.equal(map.matchInsert('ctrl-i'), 'escape', 'matching [ctrl-i] returns escape')
+    t.equal(map.matchInsert('shift-ctrl-i'), undefined, 'matching [shift-ctrl-i] returns undefined')
+
+    t.end()
+   })
+
+   t.test('\n# given I mapped [shift-ctrl-i] to ctrl-p (although that does not currently work due to readline limiations)', function (t) {
+    var map = createMap()
+
+    map.insert('shift-ctrl-i', 'ctrl-p')
+
+    t.equal(map.mappings.insert.immediates['shift-ctrl-i'], 'ctrl-p', 'it is added to the mappings under insert.immediates.shift-ctrl-i')
+    t.equal(map.matchInsert('shift-ctrl-i'), 'ctrl-p', 'matching [shift-ctrl-i] returns ctrl-p')
+    t.equal(map.matchInsert('shift-ctrl-j'), undefined, 'matching [shift-ctrl-j] returns undefined')
+    t.equal(map.matchInsert('ctrl-i'), undefined, 'matching [ctrl-i] returns undefined')
+
+    t.end()
+   })
+})
+
+test('\nkey combinations in normal mode', function (t) {
+   t.test('\n# given I mapped [ctrl-i] to escape', function (t) {
+    var map = createMap()
+
+    map.normal('ctrl-i', 'escape')
+
+    t.equal(map.mappings.normal.immediates['ctrl-i'], 'escape', 'it is added to the mappings under normal.immediates.ctrl-i')
+    t.equal(map.matchNormal('ctrl-i'), 'escape', 'matching [ctrl-i] returns escape')
+    t.equal(map.matchNormal('shift-ctrl-i'), undefined, 'matching [shift-ctrl-i] returns undefined')
+
+    t.end()
+   })
+
+   t.test('\n# given I mapped [shift-ctrl-i] to ctrl-p (although that does not currently work due to readline limiations)', function (t) {
+    var map = createMap()
+
+    map.normal('shift-ctrl-i', 'ctrl-p')
+
+    t.equal(map.mappings.normal.immediates['shift-ctrl-i'], 'ctrl-p', 'it is added to the mappings under normal.immediates.shift-ctrl-i')
+    t.equal(map.matchNormal('shift-ctrl-i'), 'ctrl-p', 'matching [shift-ctrl-i] returns ctrl-p')
+    t.equal(map.matchNormal('shift-ctrl-j'), undefined, 'matching [shift-ctrl-j] returns undefined')
+    t.equal(map.matchNormal('ctrl-i'), undefined, 'matching [ctrl-i] returns undefined')
+
+    t.end()
+   })
+})
+
+/*
+ *
+test('\nkey combinations in insert mode', function (t) {
+   t.test('\n# given I mapped [ctrl-i] to escape', function (t) {
+    var map = createMap()
+
+    map.insert('ctrl-i', 'escape')
+
+    t.equal(map.mappings.insert.immediates['ctrl-i'], 'escape', 'it is added to the mappings under insert.immediates.ctrl-i')
     t.equal(map.matchInsert({ name: 'i', ctrl: true }), 'escape', 'matching [ctrl-i] returns escape')
     t.equal(map.matchInsert({ name: 'i', ctrl: true, shift: true }), undefined, 'matching [shift-ctrl-i] returns undefined')
 
@@ -86,3 +142,4 @@ test('\nkey combinations in normal mode', function (t) {
     t.end()
    })
 })
+*/
