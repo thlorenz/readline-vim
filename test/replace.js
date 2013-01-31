b809d4145e02b220930b6b730ccbda92da8c900a
Thorsten Lorenz <thlorenz@gmx.de>
2013-01-30 21:10:45 -0500

    implementing replace (not working well for space)

--- test/replace.js
+++ test/replace.js
@@ -32,21 +32,20 @@ test('\nreplace followed by backspace', function (t) {
 
   hns.seq('r backspace')
   t.equal(hns.rli.deleteRight, 0, hns.seqed + 'does not delete right')
-  t.equal(hns.rli.ttyWrite.length, 1, 'outputs one char')
-  t.equal(hns.rli.ttyWrite[0].key.name, 'backspace', 'outputs backspace')
+  t.equal(hns.rli.ttyWrite.length, 0, 'outputs no char')
   t.equal(hns.rli.moveCursor.length, 0, 'does not tell rli to move cursor')
 
   t.end()
 })
 
-/*test('\nreplace followed by space', function (t) {
+test('\nreplace followed by space', function (t) {
 
+  // TODO: probably be better to replace with space, but due to improper key reporting (see shim key) space
+  // is currently problematic
   hns.seq('r space')
-  t.equal(hns.rli.deleteRight, 1, hns.seqed + 'deletes right once')
-  t.equal(hns.rli.ttyWrite.length, 1, 'outputs one char')
-  t.equal(hns.rli.ttyWrite[0].key.name, 'space', 'outputs space')
-  t.notOk(hns.rli.ttyWrite[0].key.shift, 'as lowercase')
-  t.equal(hns.rli.moveCursor.pop(), -1, 'moves cursor left once')
+  t.equal(hns.rli.deleteRight, 0, hns.seqed + 'does not delete right')
+  t.equal(hns.rli.ttyWrite.length, 0, 'outputs no char')
+  t.equal(hns.rli.moveCursor.length, 0, 'does not tell rli to move cursor')
 
   t.end()
-})*/
+})
